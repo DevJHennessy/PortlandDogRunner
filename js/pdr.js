66e054235da79490b0622508558aeec2f0cb465e
DevJHennessy <34386510+DevJHennessy@users.noreply.github.com>
2018-07-09 09:42:24 -0700

    Adding content to PDR website.

---
+++ js/pdr.js
@@ -0,0 +1,52 @@
+if($(window).width() > 767) {
+  $(document).ready(function () {
+      $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+                $("#aside_container").addClass("fixed_aside");
+            } else {
+                $("#aside_container").removeClass("fixed_aside");
+            }
+      });
+  });
+}
+
+else {
+$(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 300) {
+              $("#aside_container").removeClass("fixed_aside");
+            }
+            });
+        });
+
+}
+
+$(window).on('resize', function(e) {
+
+  if ($(window).width() > 767) {
+    $(document).ready(function () {
+        $(window).scroll(function () {
+          if ($(window).scrollTop() > 300) {
+                  $("#aside_container").addClass("fixed_aside");
+              } else {
+                  $("#aside_container").removeClass("fixed_aside");
+              }
+        });
+    });
+  }
+
+});
+
+$(window).on('resize', function(e) {
+
+  if ($(window).width() < 767) {
+    $(document).ready(function () {
+        $(window).scroll(function () {
+          if ($(window).scrollTop() > 300) {
+                  $("#aside_container").removeClass("fixed_aside");
+                }
+                });
+            });
+  }
+
+});
